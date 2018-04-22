35cad80a8ea61c8c6180c5d7cad91e58d7bcce03
Haseeb AH <xxxeebi@gmail.com>
2018-04-22 23:29:23 +0500

    fix users attributes

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,15 +1,15 @@
 ActiveAdmin.register User do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+  permit_params :first_name, :last_name, :company, :email, :is_active, :created_by_id
+
+  index download_links: false  do |company|
+    column :first_name
+    column :last_name
+    column :company
+    column :email
+    column :is_active, label: "Active"
+    column :created_by_id
+    actions
+  end
 
 end
